75b8c8ba2f9824c7a1c97e85825ac24b370d1a38
thedoru <thedoruv2@gmail.com>
2023-02-04 13:12:57 +0100

    Post frontend page

---
+++ pages/post/[id].js
@@ -0,0 +1,43 @@
+import NavBar from "@/components/NavBar";
+import PostLayout from "@/components/PostLayout";
+import { getAllPostsUrl, getPostData } from "@/lib/postsPublic";
+import Comments from "@/components/Comments";
+
+export default function Post({ postData, comments }) {
+  console.log(postData);
+
+  return (
+    <div>
+      <NavBar currentPage="pages" />
+      <PostLayout post={postData.post} />
+      <Comments comments={postData.comments} id={postData.post._id} />
+      <div className="mx-32 mt-5 rounded-lg border-2 p-2">
+        <h1 className="text-center font-bold">Credits</h1>
+        <a href="https://www.flaticon.com/free-icons/user" title="user icons">
+          User icons created by kmg design - Flaticon
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export async function getStaticPaths() {
+  const paths = await getAllPostsUrl();
+
+  console.log(paths);
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id, true);
+
+  return {
+    props: {
+      postData,
+    },
+  };
+}
