898ed6a75823376e9a356d24eb957fa7e43d73fd
thedoru <thedoruv2@gmail.com>
2023-02-04 13:08:29 +0100

    post page layout

---
+++ components/PostLayout.js
@@ -0,0 +1,13 @@
+export default function PostLayout({ post }) {
+  return (
+    <div className="mx-32 mt-5 rounded-lg border-2 p-2">
+      <h2 className="text-center text-xl font-semibold">{post.title}</h2>
+      <p className="text-center">by {post.author.username}</p>
+
+      <div
+        className="mt-5"
+        dangerouslySetInnerHTML={{ __html: post.content }}
+      ></div>
+    </div>
+  );
+}
