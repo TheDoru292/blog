c47edaf438dba663f001e73e3bab3ab9265e4987
thedoru <thedoruv2@gmail.com>
2023-02-04 13:20:15 +0100

    show post date

--- components/PostLayout.js
+++ components/PostLayout.js
@@ -1,8 +1,12 @@
+import { format } from "date-fns";
+
 export default function PostLayout({ post }) {
   return (
     <div className="mx-32 mt-5 rounded-lg border-2 p-2">
       <h2 className="text-center text-xl font-semibold">{post.title}</h2>
-      <p className="text-center">by {post.author.username}</p>
+      <p className="text-center">
+        by {post.author.username} on {format(new Date(post.date), "yyyy/MM/dd")}
+      </p>
 
       <div
         className="mt-5"
